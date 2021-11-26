53c17d9f404db8bfe2b91efb3e7011f8629ae94a
MrKTO444 <91790271+MrKTO444@users.noreply.github.com>
2021-11-26 14:43:06 +0000

    /

--- lab_3_task_2.py
+++ lab_3_task_2.py
@@ -3,14 +3,14 @@ import numpy as np
 H = 100
 a = np.pi / 3
 b = np.radians(30)
-from lab_3_task_1 import g
+from const import g
 
-V = (g * H * (np.tan(b) ** 2)) / (2 * (np.cos(a) **2) * (1 - (np.tan(b)) * (np.tan(a)))) ** 0.5
+V = np.sqrt(g * H * (np.tan(b) ** 2)) / (2 * (np.cos(a) **2) * (1 - (np.tan(b)) * (np.tan(a))))
 print(V)
 
 T = 200
 E = 300
-from lab_3_task_1 import k, h, e
+from const import k, h, e
 
 N = (2 / (np.pi) ** 0.5) * (h / ((k*T) ** (3/2))) * (e ** (-E / (k*T))) * (E ** (T/2))
 print(N)
